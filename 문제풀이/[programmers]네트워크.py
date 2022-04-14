e5e3887ccf9aae8d3eafc1d183e9ed2166ef47a5
Dabinnny <sdb557@naver.com>
2022-04-14 14:53:21 +0900

    dfs

---
+++ 문제풀이/[programmers]네트워크.py
@@ -0,0 +1,16 @@
+def dfs(x,computers):
+    # 들린거 표시
+    computers[x][x]=2 
+    for i in range(len(computers[x])):
+        if computers[x][i]==1 and computers[i][i]!=2: 
+            dfs(i,computers)
+    
+    
+def solution(n, computers):
+    answer = 0
+    for i in range(n):
+        # 처음 들리는 곳
+        if computers[i][i]==1: 
+            dfs(i,computers)
+            answer+=1
+    return answer
